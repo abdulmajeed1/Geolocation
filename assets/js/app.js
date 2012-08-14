fcbc185ce4bad24e9adfca1d473eb55f6bdc94c9
Chase Moody <cmoody@techhiker.com>
2012-08-13 19:37:14 -0500

    Anothe test update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,23 @@
-// 
-//  --- our app behavior logic ---
-//
-run(function () {
-    // immediately invoked on first run
-    var init = (function () {
-        if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
-        } else {
-            alert("We can reach Google - get ready for some awesome maps!");
-        }
-    })();
-    
-    // a little inline controller
-    when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
-	});
-    when('#map', function () {
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
+document.addEventListener("deviceready", init, false);
+alert('Init');
+function init() {
+	//getAccurateCurrentPosition
+	navigator.geolocation.getCurrentPosition(success, error);
+	alert('Init');
+}
 
-                x$('img#static_map').attr('src', path);
-            }, function () {
-                x$('img#static_map').attr('src', "assets/img/gpsfailed.png");
-            });
-        });
-    });
-    when('#save', function () {
-        store.save({
-            key:'config',
-            map:ui('map'),
-            zoom:ui('zoom')
-        });
-        display('#welcome');
-    });
-});
+function success(position){
+	var map = new GMaps({
+        div: '#map',
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+	
+	$('#coords').append('Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: '  + position.coords.longitude);
+}
+
+function error(error) {
+	alert(error.message);
+}
+
+// Ripple plugin is looking for config.xml file
