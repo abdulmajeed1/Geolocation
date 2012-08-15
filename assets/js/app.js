cc054262471a8812362669ffd492c49fd8021966
Chase Moody <cmoody@techhiker.com>
2012-08-14 20:03:22 -0500

    Added apk history updates to app with onready cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,27 @@
-document.addEventListener("deviceready", init, false);
-alert('Init');
+var map, lat, lng;
+
+window.addEventListener('load', function () {
+    document.addEventListener('deviceready', init, false);
+}, false);
+
 function init() {
 	//getAccurateCurrentPosition
 	navigator.geolocation.getCurrentPosition(success, error);
-	alert('Init');
 }
 
 function success(position){
-	var map = new GMaps({
+	lat = position.coords.latitude;
+	lng = position.coords.longitude;
+	
+	map = new GMaps({
         div: '#map',
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+        lat: lat,
+        lng: lng
       });
 	
-	$('#coords').append('Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: '  + position.coords.longitude);
+	$('#coords').append('Latitude: ' + lat + '<br />' + 'Longitude: '  + lng);
 }
 
 function error(error) {
 	alert(error.message);
 }
-
-// Ripple plugin is looking for config.xml file
