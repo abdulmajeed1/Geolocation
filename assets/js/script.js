5815f0471fdbbfe3c79b9941d1b628794f0a81dc
Chase Moody <cmoody@techhiker.com>
2012-10-06 09:55:35 -0500

    Changed to map multiple points

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,47 +6,34 @@ function onBodyLoad() {
 }
 
 function onDeviceReady() {
-	$( document ).delegate("#coords", "pageinit", function() {
-	  	navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
-	});
-	
+
 	$( document ).delegate("#map", "pageinit", function() {
-		navigator.geolocation.getCurrentPosition(buildMap, onError, { enableHighAccuracy: true });
+		
+		var data = [
+		  ['Person 1', -33.890542, 151.274856, 4],
+		  ['Person 2', -33.923036, 151.259052, 5],
+		  ['Chase', -34.028249, 151.157507, 3],
+		  ['Isaac', -33.80010128657071, 151.28747820854187, 2],
+		  ['Ashley', -33.950198, 151.259302, 1]
+		];
+		
+		mapAllLoc(data);
+		/*
+		$.ajax({
+		  type: "GET",
+		  dataType: "JSON",
+		  url: "http://api.chasemoody.com",
+		  success: mapAllLoc(data)
+		});*/
 	});
 	
-	$( document ).delegate("#watch", "pageinit", function() {
-		navigator.geolocation.watchPosition(onWatch, onError, { timeout: 30000, enableHighAccuracy: true });
-	});
-	
-	$( document ).delegate("#watch_map", "pageinit", function() {
-		navigator.geolocation.watchPosition(buildMap, onError, { enableHighAccuracy: true });
-	});
-	
-	$( document ).delegate("#success", "pageinit", function() {
-		alert('Success!');
-	});
-	
-	$( document ).delegate("#camera", "pageinit", function() {
-		pictureSource=navigator.camera.PictureSourceType;
-		destinationType=navigator.camera.DestinationType;
-	});
 }
 
-function onSuccess(position) {
-	$('#coords_holder').html('<p>Latitude: ' + position.coords.latitude + '</p></p>Longitude: ' + position.coords.longitude + '</p><p>Accuracy: ' + position.coords.accuracy + '</p>');
-}
+//navigator.geolocation.watchPosition(buildMap, onError, { enableHighAccuracy: true });
 
-function onWatch(position) {
-	$('#watch_holder').html('<p>Latitude: ' + position.coords.latitude + '</p></p>Longitude: ' + position.coords.longitude + '</p><p>Accuracy: ' + position.coords.accuracy + '</p>');
-}
-
-function onError(error) {
-	alert('code: '    + error.code    + '\n' +
-	'message: ' + error.message + '\n');
-}
-
-function buildMap(position){
-	var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude)
+/* Initialize Map */
+function buildMap(data){
+	var myLatlng = new google.maps.LatLng(30.26756, -97.76716);
 
 	var mapOptions = {
       center: myLatlng,
@@ -54,44 +41,20 @@ function buildMap(position){
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById('map_holder'),mapOptions);
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title:"You"
-  });
-}
-
- // Called when a photo is successfully retrieved
-//
-function onPhotoDataSuccess(imageData) {
-  // Uncomment to view the base64 encoded image data
-  // console.log(imageData);
-
-  // Get image handle
-  //
-  var smallImage = document.getElementById('smallImage');
-
-  // Unhide image elements
-  //
-  smallImage.style.display = 'block';
-
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
-
-// A button will call this function
-//
-function capturePhoto() {
-  // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-    destinationType: destinationType.DATA_URL });
+	
+    for (var i = 0; i < data.length; i++) {
+    	var loc = data[i];
+    	var myLatLng = new google.maps.LatLng(loc[1], loc[2]);
+	    var marker = new google.maps.Marker({
+	        position: myLatLng,
+	        map: map,
+	        title: loc[0],
+	        zIndex: loc[3]
+	    });
+	}
 }
 
 // Called if something bad happens.
-// 
 function onFail(message) {
   alert('Failed because: ' + message);
 }
